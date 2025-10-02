647c792ca14a9cd1601b99e1355f6d0aac457629
CsDaBest <54161308+C5DaB3st@users.noreply.github.com>
2025-10-02 13:08:23 -0500

    Update hacks.cpp

--- SauerBraten/hacks.cpp
+++ SauerBraten/hacks.cpp
@@ -1,3 +1,43 @@
 #include "hacks.h"
 
-unsigned __int64 base = reinterpret_cast<unsigned __int64>(GetModuleHandleA("sauerbraten.exe"));
+offsets* gameOffsets = nullptr;
+
+void createConsole()
+{
+	AllocConsole();
+	FILE* f;
+	freopen_s(&f, "CONOUT$", "w", stdout);
+}
+
+void setConsoleColor(int r, int g, int b)
+{
+	printf("\x1b[38;2;%d;%d;%dm", r, g, b); // foreground RGB
+}
+
+bool Init()
+{
+	if (base)
+	{
+		gameOffsets = new offsets(base);
+		//if localPlayer is false dont run the game loop because then it will crash
+		if (gameOffsets->localPlayer = reinterpret_cast<std::uint64_t*>(base + gameOffsets->localPlayer))
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+	return false;
+}
+
+void loopThroughEntList()
+{
+
+}
+
+void tpToPlayer()
+{
+
+}
