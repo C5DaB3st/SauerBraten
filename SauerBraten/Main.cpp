dfb4422718655d6916b7a7ccc7bd43a3697b7f3b
CsDaBest <54161308+C5DaB3st@users.noreply.github.com>
2025-10-02 13:08:09 -0500

    Update Main.cpp

--- SauerBraten/Main.cpp
+++ SauerBraten/Main.cpp
@@ -2,13 +2,29 @@
 
 void WINAPI hack()
 {
-	while (!GetAsyncKeyState(VK_END) && 0x01)
-	{
+	createConsole();
 
+	setConsoleColor(0, 255, 0);
+	std::cout << "INJECTED SUCCESFULLY" << std::endl;
+
+	while (true)
+	{
+		if (Init())
+		{
+		}
+		else if (GetAsyncKeyState(VK_END) && 0x01)
+		{
+			break;
+		}
+		else
+		{
+			std::cout << "INITALIZAION FAILED TRYING AGAIN! PRESS ESC TO CLOSE APP!" << std::endl;
+			continue;
+		}
 	}
 }
 
-BOOL WINAPI DllMain(HINSTANCE hInst, LPVOID lpvReserved, DWORD fdwReason)
+BOOL WINAPI DllMain(HINSTANCE hInst, DWORD fdwReason, LPVOID lpvReserved)
 {
 	switch (fdwReason)
 	{
